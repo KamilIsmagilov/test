7b6cf773d93e4a8b1117e43fd8ac5ea8d1a2fa28
Kamil <kamil.ocean@gmail.com>
2021-01-13 21:29:48 +0300

    wip

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,28 @@
 module.exports = {
-  chainWebpack: config => {
-    config.module
-      .rule('images')
-        .use('url-loader')
-          .loader('url-loader')
-          .tap(options => Object.assign(options, { 
-            outputPath: (url, resourcePath, context) => {
-              console.log('%ccontext: ', 'background: red', context)
-              if (/img/.test(context)) {
-                return `image_output_path/${url}`;
-              }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/'
+  // chainWebpack: config => {
+  //   config.module
+  //     // .rule('js')
+  //     //   .use('file-loader')
+  //     //     .loader('file-loader')
+  //     //     .tap(options => Object.assign(options, {
+  //     //       publicPath: '.'
+  //     //     }))
+  //     .rule('images')
+  //       .use('url-loader')
+  //         .loader('url-loader')
+  //         .tap(options => Object.assign(options, {
+  //           publicPath: '/test/dist',
+  //           // outputPath: (url, resourcePath, context) => {
+  //           //   console.log('%ccontext: ', 'background: red', context)
+  //           //   if (/img/.test(context)) {
+  //           //     return `image_output_path/${url}`;
+  //           //   }
 
-              return `output_path/${url}`;
-              }
-           }))
-  }
+  //           //   return `output_path/${url}`;
+  //           //   }
+  //          }))
+  // }
 }
